d5885969ddf19004eab7147741c4311bdcb9a360
Johan Bency <jbency@overstock.com>
2021-02-28 23:50:18 +0000

    Adding necessary dependencies

--- src/main/java/com/johan/project/mathoperationservice/MathOperationServiceApplication.java
+++ src/main/java/com/johan/project/mathoperationservice/MathOperationServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MathOperationServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MathOperationServiceApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/johan/project/mathoperationservice/aop/NoLogging.java
@@ -0,0 +1,8 @@
+package com.johan.project.mathoperationservice.aop;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface NoLogging {
+}
